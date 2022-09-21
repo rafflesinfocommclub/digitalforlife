d6c06645b86a7a185c2b44d7852d35ff0da6bb5c
rafflesinfocommclub <114056656+rafflesinfocommclub@users.noreply.github.com>
2022-09-21 16:37:16 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,24 @@
+let questions = [
+  {
+    name: "Which of the following is not a way to cope with stress",
+    options: ["Stay silent", "Pop bubble wrap", "Exercise", "Read books"],
+    answer: 1
+  }, 
+  {
+    name: "There is a method to cope with stress where you inhale, hold your breath and exhale for a number of seconds. What's this method called?",
+    options: ["7-6-5 rule", "6-7-5 rule", "7-5-6 rule", "6-5-7 rule"],
+    answer: 3
+  },
+  {
+    name: "Who should you first approach when you need help with coping stress?",
+    options: ["Friends", "Parents", "Police", "All of the above"],
+    answer: 2
+  },
+  {
+    name: "Which of the following is not an effect of stress?",
+    options: ["Depression", "Headache", "High blood pressure", "Covid-19"],
+    answer: 4
+  }
+]
+
+export { questions }
